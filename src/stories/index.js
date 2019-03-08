7c61b17a1948938ff4c9fdc649665d83601d4bbe
Jelizaveta Mac <jelizaveta.potapova@devbridge.com>
2019-03-08 09:21:27 +0200

    Add most simple static component

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,3 +1,9 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
+
+import Happy from '../components/Happy';
+
+// 1 most simple case - simple static component
+storiesOf('Success')
+  .add('animation', () => <Happy />);
